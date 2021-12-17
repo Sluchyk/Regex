5a8a07a110704f6c20183f691cb242228d2bf47c
Sluchyk Vasyl <boss.slucyk@gmail.com>
2021-12-17 17:24:21 +0200

    1

--- src/View.java
+++ src/View.java
@@ -2,4 +2,15 @@ public class View {
     private final String  WRONG_DATA="WRONG DATA";
     private final  String NICKNAME="NICKNAME: ";
     private  final String NAME="NAME: ";
+    public void printMessage (String message){
+        System.out.println(message);
+    }
+
+    public String concatenationString (String... message){
+        StringBuilder concatString = new StringBuilder();
+        for (String v : message){
+            concatString = concatString.append(v);
+        }
+        return new String(concatString);
+    }
 }
